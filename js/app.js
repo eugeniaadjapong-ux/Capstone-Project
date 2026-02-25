b601698c46b04276dbde66e13cb13c7c902a3935
Eugenia Adjapong <eugeniaadjapong@gmail.com>
2026-02-25 09:22:52 +0000

    Integrated weather API functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-function searchWeather(){
+const apiKey = "44bc83bb3ca0223412f2001862321cd7";
+
+async function searchWeather(){
     const city = document.getElementById("cityInput").value;
 
     if(city === ""){
@@ -6,8 +8,29 @@ function searchWeather(){
         return;
     }
 
-    document.getElementById("errorMessage").innerText = "";
-    document.getElementById("weatherCard").style.display = "block";
+    try{
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+        );
 
-    document.getElementById("cityName").innerText = city;
+        if(!response.ok){
+            throw new Error("City not found");
+        }
+
+        const data = await response.json();
+
+        document.getElementById("weatherCard").style.display = "block";
+        document.getElementById("errorMessage").innerText = "";
+
+        document.getElementById("cityName").innerText = data.name;
+        document.getElementById("temperature").innerText =
+            "Temperature: " + data.main.temp + "°C";
+
+        document.getElementById("condition").innerText =
+            data.weather[0].description;
+
+    } catch(error){
+        document.getElementById("errorMessage").innerText =
+            "City not found or network error";
+    }
 }
